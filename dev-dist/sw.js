20ee7b42a72a718c9e8732bff02780974e559043
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-08 14:17:05 +0100

    update input fields

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b7fccfec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ju8b0j9jaho"
+    "revision": "0.i95lv5d5tog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
