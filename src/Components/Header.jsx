7efd9bacf219c7baeab4cc06c2b23cc381af638e
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-10 10:44:21 +0100

    change favicons and logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import classes from './Header.module.css';
-import { ReactComponent as Logo } from '../images/logo.svg'
+// import { ReactComponent as Logo } from '../images/logo.svg'
+import imageUrl from '../images/logo.png';
 
 function Header() {
     return (
         <header>
-            <Logo className={classes.logo} />
+            <img src={imageUrl} width="270" height="80" className={classes.logo} alt="logo"></img>
         </header>
     )
 }
