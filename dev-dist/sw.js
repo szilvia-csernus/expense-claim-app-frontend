625e891510945e31ac21786ac5eb4d787b2102af
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-01-08 10:12:26 +0000

    edit file upload button

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b7fccfec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sm0vakorou8"
+    "revision": "0.n3p3nifrpko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
