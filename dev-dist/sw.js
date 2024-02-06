18ae59f0f7a37631363862e4f599a467e2811837
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-06 18:38:14 +0000

    fix file upload issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cea655ed'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5nhebs8bdv8"
+    "revision": "0.jp1d1n9mjog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
