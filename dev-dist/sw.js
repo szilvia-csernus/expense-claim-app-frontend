464c5eaa754b5523b45115251c4d97a181c1e440
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-18 18:35:07 +0000

    edit IBAN field text

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cea655ed'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.edjiu70b1eg"
+    "revision": "0.81b523t93m8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
